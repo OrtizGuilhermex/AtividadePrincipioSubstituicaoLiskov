75a63e7f4d5b8f7a43e6434e5ea717163b55eac0
OrtizGuilhermex <luiz_gf_ortiz@estudante.sesisenai.org.br>
2025-12-03 15:55:29 -0300

    feat: adiciona teste main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Pedido;
+import service.EstrategiaDeFrete;
+import service.FreteGratis;
+import service.FretePadrao;
+import service.ProcessadorDePagamento;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ProcessadorDePagamento processadorDePagamento = new ProcessadorDePagamento();
+        Pedido pedidoBase = new Pedido(100.0);
+
+        EstrategiaDeFrete fretePadrao = new FretePadrao(pedidoBase);
+        processadorDePagamento.processar(pedidoBase, fretePadrao);
+
+        EstrategiaDeFrete freteGratis = new FreteGratis();
+        processadorDePagamento.processar(pedidoBase,freteGratis);
+
+
     }
 }
