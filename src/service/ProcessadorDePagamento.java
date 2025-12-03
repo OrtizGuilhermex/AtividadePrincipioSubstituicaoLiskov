f36b2782ded755155ec0adbf9d6d2647483902ed
OrtizGuilhermex <luiz_gf_ortiz@estudante.sesisenai.org.br>
2025-12-03 15:54:36 -0300

    feat: adiciona classe para processarPagamento

--- src/service/ProcessadorDePagamento.java
+++ src/service/ProcessadorDePagamento.java
@@ -3,12 +3,17 @@ package service;
 import model.Pedido;
 
 public class ProcessadorDePagamento {
-    public void processar(Pedido pedido, double freteBase) {
-        pedido.aplicarFrete(freteBase);
-        if (pedido.getValorFinal() < pedido.getValorBruto()) {
+    public double processar(Pedido pedido, EstrategiaDeFrete estrategiaDeFrete) {
 
+        double valorFinal = estrategiaDeFrete.calcularValorFinal(pedido);
+
+        if (valorFinal < pedido.getValorBruto()) {
             throw new IllegalStateException("Erro de contrato: O valor final não pode ser menor que o valor bruto após a aplicação do frete.");
         }
+
+        System.out.println("Pedido processado. Valor Bruto: " + pedido.getValorBruto() + " | Valor Final: " + valorFinal + " | usando estratégia: " + estrategiaDeFrete.getClass().getSimpleName());
+
+        return valorFinal;
     }
 
 }
