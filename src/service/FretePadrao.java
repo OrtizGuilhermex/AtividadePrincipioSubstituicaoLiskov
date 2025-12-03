777cbbde74ea6dceb33ddf5fbf5d44b543ad0c6a
OrtizGuilhermex <luiz_gf_ortiz@estudante.sesisenai.org.br>
2025-12-03 15:55:21 -0300

    feat: adiciona classe FretePadrao

--- src/service/FretePadrao.java
+++ src/service/FretePadrao.java
@@ -1,4 +1,17 @@
 package service;
 
-public class FretePadrao {
+import model.Pedido;
+
+public class FretePadrao implements EstrategiaDeFrete{
+
+    public FretePadrao(Pedido pedido) {
+        if(pedido.getValorFrete() < 0){
+            throw new IllegalArgumentException("Valor do frete não pode ser negativa!");
+        }
+    }
+
+    @Override
+    public double calcularValorFinal(Pedido pedido) {
+        return pedido.getValorBruto() + pedido.getValorFrete();
+    }
 }
